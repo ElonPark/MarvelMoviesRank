9c8c20c894c55a09d5190497f92a029b80759af5
Elon <sungwoon.park92@gmail.com>
2019-03-07 02:27:30 +0900

    :heavy_plus_sign: 프로젝트 추가 및 기본 셋팅, 라이브러리 추가

---
+++ MarvelMoviesRank/MarvelMoviesRank/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  MarvelMoviesRank
+//
+//  Created by Elon on 07/03/2019.
+//  Copyright © 2019 Elon. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
